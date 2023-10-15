Fix layer normalization

nn/transformer.py
@@ -131,11 +131,11 @@ def __transformer_encoder_forward(parameters: dict,
 
     print(f"[Encoder] Layer data shape: {input_data.shape}")
 
-    attn_output, _ = attention_forward(
+    attention_output, _ = attention_forward(
         parameters['attention'], input_data, input_data, input_data,
         config, mask)
 
-    add_1 = jnp.add(attn_output, input_data)
+    add_1 = jnp.add(attention_output, input_data)
     print(f"[Encoder] Add layer 1: {add_1.shape}")
 
     layer_norm_1 = BATCH_LAYER_NORM(parameters['norm-1'], add_1)

nn/utils.py
@@ -47,10 +47,10 @@ def init_layer_norm(rng_key: random.PRNGKey, dimensions: int):
     rng_key, *_keys = random.split(rng_key, num=3)
     weight_key, bias_key = _keys
 
-    scale = random.normal(weight_key, (dimensions, ))
-    offset = random.normal(bias_key, (dimensions, ))
+    weights = random.normal(weight_key, (dimensions, dimensions))
+    biases = random.normal(bias_key, (dimensions, ))
 
-    parameters = {'layer-norm': (scale, offset)}
+    parameters = {'layer-norm': (weights, biases)}
 
     return rng_key, parameters
 
@@ -84,7 +84,7 @@ def __forward_layer_norm(parameters: dict, input_data):
     output = (input_data - mean) / jnp.sqrt(variance - EPSILON)
 
     # print(params["layer-norm"][0].shape,  params["layer-norm"][1].shape)
-    # print("Layer norm output shape: ", layer_norm_output.shape)
+    print(f"Layer norm output shape {input_data.shape} - {output.shape}")
 
     output = jnp.dot(output, parameters['layer-norm'][0]) \
         + parameters['layer-norm'][1]